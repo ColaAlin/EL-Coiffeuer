7d3132450b91eb0ca4372fc1b277e5e00cb49b44
AlinCola <colaalincristian@gmail.com>
2023-05-13 15:52:39 +0200

    first commit 70% ready

---
+++ javaScript.js
@@ -0,0 +1,448 @@
+// Get all the dropdown from document
+document.querySelectorAll(".dropdown-toggle").forEach(dropDownFunc);
+
+// Dropdown Open and Close function
+function dropDownFunc(dropDown) {
+  console.log(dropDown.classList.contains("click-dropdown"));
+
+  if (dropDown.classList.contains("click-dropdown") === true) {
+    dropDown.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      if (
+        this.nextElementSibling.classList.contains("dropdown-active") === true
+      ) {
+        // Close the clicked dropdown
+        this.parentElement.classList.remove("dropdown-open");
+        this.nextElementSibling.classList.remove("dropdown-active");
+      } else {
+        // Close the opened dropdown
+        closeDropdown();
+
+        // add the open and active class(Opening the DropDown)
+        this.parentElement.classList.add("dropdown-open");
+        this.nextElementSibling.classList.add("dropdown-active");
+      }
+    });
+  }
+
+  if (dropDown.classList.contains("hover-dropdown") === true) {
+    dropDown.onmouseover = dropDown.onmouseout = dropdownHover;
+
+    function dropdownHover(e) {
+      if (e.type == "mouseover") {
+        // Close the opened dropdown
+        closeDropdown();
+
+        // add the open and active class(Opening the DropDown)
+        this.parentElement.classList.add("dropdown-open");
+        this.nextElementSibling.classList.add("dropdown-active");
+      }
+
+      // if(e.type == 'mouseout'){
+      //     // close the dropdown after user leave the list
+      //     e.target.nextElementSibling.onmouseleave = closeDropdown;
+      // }
+    }
+  }
+}
+
+// Listen to the doc click
+window.addEventListener("click", function (e) {
+  // Close the menu if click happen outside menu
+  if (e.target.closest(".dropdown-container") === null) {
+    // Close the opened dropdown
+    closeDropdown();
+  }
+});
+
+// Close the opened Dropdowns
+function closeDropdown() {
+  console.log("run");
+
+  // remove the open and active class from other opened Dropdown (Closing the opened DropDown)
+  document
+    .querySelectorAll(".dropdown-container")
+    .forEach(function (container) {
+      container.classList.remove("dropdown-open");
+    });
+
+  document.querySelectorAll(".dropdown-menu").forEach(function (menu) {
+    menu.classList.remove("dropdown-active");
+  });
+}
+
+// close the dropdown on mouse out from the dropdown list
+document.querySelectorAll(".dropdown-menu").forEach(function (dropDownList) {
+  // close the dropdown after user leave the list
+  dropDownList.onmouseleave = closeDropdown;
+});
+
+const url = "extern/preisMen.json";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  });
+
+function appendData(data) {
+  const home = document.querySelector("#content");
+  document.querySelector("#man").addEventListener("click", () => {
+    while (home.hasChildNodes()) {
+      home.removeChild(home.firstChild);
+    }
+    data.forEach(myFunction);
+    function myFunction(item) {
+      home.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" + item.hair + "  " + "<br>" + item.preis + "</li>" + "<br>"
+      );
+    }
+  });
+}
+
+const frau = "extern/preisWoman.json";
+
+fetch(frau)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendDataFrau(data);
+  });
+
+function appendDataFrau(data) {
+  const home2 = document.querySelector("#content");
+  document.querySelector("#frau").addEventListener("click", () => {
+    while (home2.hasChildNodes()) {
+      home2.removeChild(home2.firstChild);
+    }
+    data.forEach(myFunction);
+    function myFunction(item) {
+      home2.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" + item.hair + "  " + "<br>" + item.preis + "</li>" + "<br>"
+      );
+    }
+  });
+}
+const kid = "extern/preisKids.json";
+
+fetch(kid)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendDataKid(data);
+  });
+function appendDataKid(data) {
+  const home3 = document.querySelector("#content");
+  document.querySelector("#kind").addEventListener("click", () => {
+    while (home3.hasChildNodes()) {
+      home3.removeChild(home3.firstChild);
+    }
+    data.forEach(myFunction);
+    function myFunction(item) {
+      home3.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          item.geschlecht +
+          "<br>" +
+          "<br>" +
+          item.alter +
+          "<br>" +
+          item.preis +
+          "<br>" +
+          "<br>" +
+          item.alter2 +
+          "<br>" +
+          item.preis2 +
+          "<br>" +
+          "<br>" +
+          item.sonnst +
+          "</li>" +
+          "<br>"
+      );
+    }
+  });
+}
+
+const cosmetics = "extern/preisCosmetics.json";
+fetch(cosmetics)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendDataCosmetics(data);
+  });
+
+function appendDataCosmetics(data) {
+  const behaEins = data.behandlungEins;
+  const behaZwei = data.behandlungZwei;
+  const product = data.text;
+  const extra = data.beautyExtras;
+
+  const home4 = document.querySelector("#content");
+  document.querySelector("#cosmo").addEventListener("click", () => {
+    while (home4.hasChildNodes()) {
+      home4.removeChild(home4.firstChild);
+    }
+    for (i of product) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<p>" + i.product + "</p>" + "<br>"
+      );
+    }
+
+    behaEins.forEach(oneFunction);
+
+    function oneFunction(item) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          item.name +
+          "<br>" +
+          "<br>" +
+          item.description +
+          "<br>" +
+          "<br>" +
+          item.preis +
+          "</li>" +
+          "<br>"
+      );
+    }
+    behaZwei.forEach(twoFunction);
+    function twoFunction(item) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.name +
+          "<br>" +
+          "<br>" +
+          item.description +
+          "<br>" +
+          "<br>" +
+          item.preis +
+          "</li>" +
+          "<br>"
+      );
+    }
+    extra.forEach(extraFunction);
+    function extraFunction(item) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.description +
+          "<br>" +
+          "<br>" +
+          item.aquapeeling +
+          "<br>" +
+          item.aquapeelingPreis +
+          "<br>" +
+          "<br>" +
+          item.microneedling +
+          "<br>" +
+          item.microneedlingPreis +
+          "<br>" +
+          "<br>" +
+          item.radiofrequenz +
+          "<br>" +
+          item.radiofrequenzPreis +
+          "<br>" +
+          "<br>" +
+          item.ionenlifting +
+          "<br>" +
+          item.ionenliftingPreis +
+          "<br>" +
+          "<br>" +
+          item.ultraschall +
+          "<br>" +
+          item.ultraschallPreis +
+          "<br>" +
+          "</li>" +
+          "<br>"
+      );
+    }
+  });
+}
+
+const permanent = "extern/permanentMakeup.json";
+fetch(permanent)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendDataCosmeticsExtra(data);
+  });
+
+function appendDataCosmeticsExtra(data) {
+  const makeUp = data.permanentMakeup;
+
+  const home4 = document.querySelector("#content");
+  document.querySelector("#makeup").addEventListener("click", () => {
+    while (home4.hasChildNodes()) {
+      home4.removeChild(home4.firstChild);
+    }
+    makeUp.forEach(myFunction);
+
+    function myFunction(item) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.description +
+          "<br>" +
+          "<br>" +
+          item.powder +
+          "<br>" +
+          item.powderPreis +
+          "<br>" +
+          item.oneYear +
+          "<br>" +
+          item.oneYearPreis +
+          "<br>" +
+          "</li>" +
+          "<br>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.microblading +
+          "<br>" +
+          item.microPreis +
+          "<br>" +
+          item.oneYear2 +
+          "<br>" +
+          item.oneYearPreis2 +
+          "</li>" +
+          "<br>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.lips +
+          "<br>" +
+          item.lipsPreis +
+          "<br>" +
+          item.oneYear3 +
+          "<br>" +
+          item.oneYearPreis3 +
+          "</li>" +
+          "<br>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.lidstrich +
+          "<br>" +
+          item.lidstrichPreis +
+          "<br>" +
+          item.oneYear4 +
+          "<br>" +
+          item.oneYearPreis4 +
+          "</li>" +
+          "<br>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.shading +
+          "<br>" +
+          item.shadingPreis +
+          "<br>" +
+          item.oneYear5 +
+          "<br>" +
+          item.oneYearPreis5 +
+          "</li>" +
+          "<br>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.wimpern +
+          "<br>" +
+          item.wimpernPreis +
+          "<br>" +
+          item.oneYear6 +
+          "<br>" +
+          item.oneYearPreis6 +
+          "</li>" +
+          "<br>"
+      );
+    }
+  });
+}
+const eyesMakeup = "extern/shoeneAugen.json";
+fetch(eyesMakeup)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendDataEyes(data);
+  });
+
+function appendDataEyes(data) {
+  const eyesmakeup = data.shoeneAugen;
+
+  const home4 = document.querySelector("#content");
+  document.querySelector("#eyes").addEventListener("click", () => {
+    while (home4.hasChildNodes()) {
+      home4.removeChild(home4.firstChild);
+    }
+    eyesmakeup.forEach(eyeFunction);
+
+    function eyeFunction(item) {
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.description +
+          "<br>" +
+          "<br>" +
+          item.brow +
+          "<br>" +
+          item.browPreis +
+          "</li>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" + "<br>" + item.lash + "<br>" + item.lashPreis + "</li>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" + "<br>" + item.wimpern + "<br>" + item.wimpernPreis + "</li>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" + "<br>" + item.augen + "<br>" + item.augenPreis + "</li>"
+      );
+      home4.insertAdjacentHTML(
+        "afterbegin",
+        "<li>" +
+          "<br>" +
+          item.augenKorrektur +
+          "<br>" +
+          item.augenKorrekturPreis +
+          "</li>"
+      );
+    }
+  });
+}
+
+const content = document.getElementById("content");
+
+document.addEventListener("click", (event) => {
+  if (!event.target.closest("ul")) {
+    content.innerText = "";
+  }
+});
