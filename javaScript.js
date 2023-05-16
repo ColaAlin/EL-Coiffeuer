0eec468013f6569ea88ccbbef9dc719141a6beb8
AlinCola <colaalincristian@gmail.com>
2023-05-16 09:47:18 +0200

    toutchstart not working

--- javaScript.js
+++ javaScript.js
@@ -6,7 +6,7 @@ function dropDownFunc(dropDown) {
   console.log(dropDown.classList.contains("click-dropdown"));
 
   if (dropDown.classList.contains("click-dropdown") === true) {
-    dropDown.addEventListener("touchstart", function (e) {
+    dropDown.addEventListener("click", function (e) {
       e.preventDefault();
 
       if (
