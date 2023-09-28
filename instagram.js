3bbffa9920e0993d10be0b6922c931036f2677cf
AlinCola <colaalincristian@gmail.com>
2023-09-28 09:59:39 +0300

    ignore file instagram.js

--- instagram.js
+++ instagram.js
@@ -3,8 +3,7 @@ let userFeed = new Instafeed({
   userId: "el_coiffeure_cosmetics",
   target: "instafeed-container",
   resolution: "low_resolution",
-  accessToken:
-    "IGQVJWREN3TlhhNHJIMVctcFlfR1RrVlUxQnZACLUIyWl9fRlkyYkExMnlOT1BvT3g5WlFOWkY0UEZAFVkxIRVUwYlZAKVjBxQkhOdXR6US1PNzlGbXBoYk95M1JpNTJNQzJpWVNHVm05TmlIN3B4ZAzFrdQZDZD",
+  accessToken: "",
   limit: 12,
   template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
 });
